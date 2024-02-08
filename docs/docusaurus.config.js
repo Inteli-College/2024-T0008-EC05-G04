cae6dd6a04f7ef69c6258a615e2bf3e1ba39a501
Olin <olin.costa@sou.inteli.edu.br>
2024-02-08 16:57:46 -0300

    fix: yaml files

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'Ólin',
+        title: 'Grupo 4',
         logo: {
           alt: 'My Site Logo',
           src: 'img/galaxy.webp',
