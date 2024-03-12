5e7f3a367fb5c2f3b663c37789a0a80160fc07b0
ItsVasconcellos <fernando.asv@uol.com.br>
2024-03-12 17:45:30 -0300

    feat: restartign the program

--- src/robot/src/app.py
+++ src/robot/src/app.py
@@ -4,36 +4,33 @@ from dobotController import DobotController
 from position import Position
 import json
 
-app = Flask(__name__)
 dobot_controller = DobotController()
 
+available_ports = dobot_controller.list_ports()
 
-@app.route('/')
-def main():
-    available_ports = dobot_controller.list_ports()
+port = inquirer.prompt(
+    [inquirer.List("port", message="Select the port", choices=available_ports)]
+).get("port")
+dobot_controller.connect(port)
 
-    port = inquirer.prompt(
-        [inquirer.List("port", message="Select the port", choices=available_ports)]
-    ).get("port")
-    dobot_controller.connect(port)
+while True:
+    pass
 
-@app.route('/make-kits')
+
+# Fazer os kits
 def make_kits(item_list):
-    items_kit = item_list.loads()
-    for item in items_kit:
-        dobot_controller.move_to(Position(item.position.get("X"), item.position.get("Y"), 
-        dobot_controller.home_position.z, item.position.get("R")), wait)
-        
-        dobot_controller.move_to(Position(item.position.get("X"), item.position.get("Y"), 
-        item.position.get("Z"), item.position.get("R")), wait)
-        
-        dobot_controller.enable_tool()
+        items_kit = item_list.loads()
+        for item in items_kit:
+            dobot_controller.move_to(Position(item.position.get("X"), item.position.get("Y"), 
+            dobot_controller.home_position.z, item.position.get("R")), wait)
+            
+            dobot_controller.move_to(Position(item.position.get("X"), item.position.get("Y"), 
+            item.position.get("Z"), item.position.get("R")), wait)
+            
+            dobot_controller.enable_tool()
 
-        dobot_controller.move_to(Position(item.position.get("X"), item.position.get("Y"), 
-        dobot_controller.home_position.z, item.position.get("R")), wait)
-        
-        dobot_controller.home()
-        
-
-if __name__ == '__main__':
-    app.run()
+            dobot_controller.move_to(Position(item.position.get("X"), item.position.get("Y"), 
+            dobot_controller.home_position.z, item.position.get("R")), wait)
+            
+            dobot_controller.home()
+            
