f71057759348df9544a848e35f49ec5449532e40
lidiamariano <lidia.mariano@sou.inteli.edu.br>
2024-03-14 13:11:24 -0300

    feat: add def make-kits

--- src/robot/src/app.py
+++ src/robot/src/app.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 from flask_cors import CORS
 import inquirer
 from yaspin import yaspin
+import json
+
 
 from dobotController import DobotController
 from position import Position
@@ -10,8 +12,12 @@ ROBOT_ID = "001"
 
 dobot_controller = DobotController()
 
+with open("home.json") as file:
+    home = json.load(file)
+
 app = Flask(__name__)
 CORS(app)
+
 
 
 def parse_position(data):
@@ -34,23 +40,24 @@ def register_robot():
     # registra o robô no backend
     pass
 
+#endpoint para montar kit
+@app.route('/make-kits')
+def make_kits(item_list):
+    #carregando json para o objeto
+    items_kit = item_list.loads()
+    for item in items_kit.positions:
+        dobot_controller.move_to(Position(item.get("X"), item.get("Y"),
+        dobot_controller.home_z, item.get("R")), wait=200)
 
-@app.route("/ping", methods=["GET"])
-def ping():
-    return "pong"
+        dobot_controller.move_to(Position(item.get("X"), item.get("Y"),
+        item.get("Z"), item.get("R")), wait=200)
 
+        dobot_controller.enable_tool()
 
-@app.route("/move", methods=["POST"])
-def move_linear():
-    data = request.json
-    target_position = parse_position(data)
-    dobot_controller.move_to(target_position, wait=data.get("wait", True))
+        dobot_controller.move_to(Position(item.get("X"), item.get("Y"),
+        dobot_controller.home_z, item.get("R")), wait=200)
 
-    return "ok"
-
-
-# execute
-
+        dobot_controller.move_to(Position(item.get(home["X"]), item.get(home["Y"]), item.get(home["Z"]), item.get(home["R"])), wait=200)
 
 if __name__ == "__main__":
     available_ports = dobot_controller.list_ports()
