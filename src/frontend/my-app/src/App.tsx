4e731f3f4585e7519f671920fd46267e8a72df83
Olin <olin.costa@sou.inteli.edu.br>
2024-03-15 00:10:49 -0300

    add: login page, menu page and integration with tailwind

--- src/frontend/my-app/src/App.tsx
+++ src/frontend/my-app/src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/login';
+import Menu from './pages/menu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/about" element={<Menu />} />
+        // Add more routes as needed
+      </Routes>
+    </Router>
   );
 }
 
