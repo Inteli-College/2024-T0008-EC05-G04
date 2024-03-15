18196ee0dbc17304be20fdcdc654ece0b0258679
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2024-03-15 12:18:08 -0300

    feat: raspberry autorun

--- src/firmware/src/main.py
+++ src/firmware/src/main.py
@@ -1,28 +1,12 @@
 import urequests as requests
 import network
-from machine import Pin
+import machine
 import time
 
 import tokens
 
-# Pino GPIO conectado ao sensor óptico reflexivo
-pino_sensor = machine.ADC(26)
-
-# Pino GPIO conectado ao LED
-pino_led = machine.Pin(12, machine.Pin.OUT)
-pino_buz = machine.Pin(13, machine.Pin.OUT)
-
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(tokens.SSID, tokens.PASSWORD)
-
-while not wlan.isconnected():
-    pass
-print("Connected to wifi")
-
 # URL do endpoint local para enviar o POST
 local_url = f"{tokens.BASE_URL}/raspberry-feed"  # Replace with your local URL
-print(local_url)
 
 
 # Função para enviar o POST com o valor do sensor
@@ -33,20 +17,36 @@ def send_post(distance_measured):
     print(response.text)
 
 
-while True:
-    distance_measured = pino_sensor.read_u16()
-    print(distance_measured)
+def main():
+    # Pino GPIO conectado ao sensor óptico reflexivo
+    pino_sensor = machine.ADC(26)
 
-    if distance_measured <= 25000:
-        print("Objeto: Detectado")
-        pino_led.on()
-        pino_buz.on()
-    else:
-        print("Objeto: Ausente")
-        pino_led.off()
-        pino_buz.off()
+    # Pino GPIO conectado ao LED
+    pino_led = machine.Pin(12, machine.Pin.OUT)
+    pino_buz = machine.Pin(13, machine.Pin.OUT)
 
-    # Send the sensor value to the local URL
-    send_post(distance_measured)
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(tokens.SSID, tokens.PASSWORD)
 
-    time.sleep(1)
+    while not wlan.isconnected():
+        pass
+    print("Connected to wifi")
+
+    while True:
+        distance_measured = pino_sensor.read_u16()
+        print(distance_measured)
+
+        if distance_measured <= 25000:
+            print("Objeto: Detectado")
+            pino_led.on()
+            pino_buz.on()
+        else:
+            print("Objeto: Ausente")
+            pino_led.off()
+            pino_buz.off()
+
+        # Send the sensor value to the local URL
+        send_post(distance_measured)
+
+        time.sleep(1)
