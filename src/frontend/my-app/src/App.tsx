d357ad3ebf2827bf8cafa396e4303fdffaeb2d68
Cecilia <cecilia.goncalves@sou.inteli.edu.br>
2024-03-25 17:24:59 -0300

    Update App.tsx

--- src/frontend/my-app/src/App.tsx
+++ src/frontend/my-app/src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/login';
 import Menu from './pages/menuFunc';
 import VisuKits from './pages/visualKits'
-import MontarKit from './pages/montarKits';
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <Route path="/" element={<Login/>} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/visuKits" element={<VisuKits />} />
-        <Route path="/montarKits/*" element={<MontarKit />} />
         // Add more routes as needed
       </Routes>
     </Router>
