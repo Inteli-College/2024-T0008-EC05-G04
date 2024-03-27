d8f68f1792d8ab4d1bebf3d20d2d1cb5f72bdcbb
lidiamariano <lidia.mariano@sou.inteli.edu.br>
2024-03-27 15:56:56 -0300

    feat: ajustes de tamanho

--- src/frontend/my-app/src/components/inputCadastroKits.tsx
+++ src/frontend/my-app/src/components/inputCadastroKits.tsx
@@ -10,12 +10,12 @@ interface InputBottomCadastroKitsProps{
 const InputBottomCadastroKits: React.FC<InputBottomCadastroKitsProps>  = ({text, label, props}) => {
     return(
         <div>
-            <div className='flex items-center mb-6 ml-0'>
-                <div className='text-[20px]'>
-                    <p className= "font-medium text-[20px]" >{text}</p> 
+            <div className='flex items-center mb-4 ml-0'>
+                <div className='text-[16px]'>
+                    <p className= "font-medium text-[16px]" >{text}</p> 
                 </div>
                 <div className='ml-12 w-1/2'>
-                    <TextField id="outlined-basic" label={label} className={`rounded-full ${props}`} />
+                    <TextField id="outlined-basic" label={label} className={`${props}`} />
                 </div>
             </div>
         </div>
--- src/frontend/my-app/src/pages/cadastroKit.tsx
+++ src/frontend/my-app/src/pages/cadastroKit.tsx
@@ -6,12 +6,12 @@ const CadastroKit: React.FC = () =>{
     return (
         <>
         <div className="flex justify-center">
-            <h1 className="font-bold text-[36px] mb-5">Cadastrar Layout</h1>
+            <h1 className="font-bold text-[28px] mb-0">Cadastrar Layout</h1>
         </div>
         <div className="flex flex-col items-center">
             <div className="w-full flex flex-col items-center">
                 <InputCadastroKits props="w-[760px]" text="Nome do Kit" label="Digite o nome do kit"/>
-                <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4 mt-5">
+                <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4 mt-0">
                     <CardItem title="Item 1"/>
                     <CardItem title="Item 2"/>
                     <CardItem title="Item 3"/>
@@ -21,11 +21,11 @@ const CadastroKit: React.FC = () =>{
                     <CardItem title="Item 7"/>
                     <CardItem title="Item 8"/>  
                 </div>
-            <div className="mt-8 flex gap-24">
+            <div className="mt-4 flex gap-24">
                 <InputCadastroKits props="w-72"text="Item" label="Digite o item a ser adicionado"/>
                 <InputCadastroKits props="w-72"text="Quantidade" label="Digite a quantidade desse item"/>
             </div>
-            <div className="flex gap-40 mb-6">
+            <div className="flex gap-40 mb-4">
                 <ButtonMedio title="Salvar" props="bg-blue-900 w-28"/>
                 <ButtonMedio title="Cancelar" props="bg-red-500 w-36"/>
             </div>
