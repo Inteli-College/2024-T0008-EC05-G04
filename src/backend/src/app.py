e4c1cffb73b2b6e45ea71830d15038150da6fd6f
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2024-03-28 14:02:31 -0300

    fix: add cors

--- src/backend/src/app.py
+++ src/backend/src/app.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 import routers
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(routers.router, prefix="/api")
 
