2d35a02690b4bf590bd35d9f84c7510ea1372950
Olin <olin.costa@sou.inteli.edu.br>
2024-03-27 21:38:28 -0300

    feat: adds robot and kits dropdown autocomplete fetch

--- src/frontend/my-app/src/pages/menuFunc.tsx
+++ src/frontend/my-app/src/pages/menuFunc.tsx
@@ -16,7 +16,7 @@ const MainPage: React.FC = () => {
     {
       title: 'Criar Kit',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      path: '/',
+      path: '/montarKits',
     },
     {
       title: 'Estatísticas',
