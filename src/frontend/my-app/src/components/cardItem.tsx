eb3fbce2e4db7c79732a91aff86bad080ea0b120
ItsVasconcellos <fernando.asv@uol.com.br>
2024-03-27 23:01:53 -0300

    feat: logic for saving item clicked

--- src/frontend/my-app/src/components/cardItem.tsx
+++ src/frontend/my-app/src/components/cardItem.tsx
@@ -1,18 +1,14 @@
-import React from 'react';
+import { useContext, useState } from "react";
 
 interface CardItemProps{
     number: number
+    onSelectItem: (item: number) => void;
 }
 
-const CardItem: React.FC<CardItemProps>  = ({number}) => {
-    
-    function clickItem(){
-        
-
-    }
+const CardItem: React.FC<CardItemProps>  = ({number,onSelectItem}) => {
     return(
         <div>
-            <div className="bg-white rounded-lg border border-black shadow w-52 h-52 flex items-center justify-center transition duration-500  hover:text-violet-600" onClick={clickItem}>
+            <div className="bg-white rounded-lg border border-black shadow w-52 h-52 flex items-center justify-center transition duration-500  hover:text-violet-600" onClick={() => onSelectItem(number)}>
                     <p className="text-[22px] black">
                         item {number}
                     </p>
--- src/frontend/my-app/src/pages/cadastroKit.tsx
+++ src/frontend/my-app/src/pages/cadastroKit.tsx
@@ -1,8 +1,23 @@
 import CardItem from "../components/cardItem";
 import InputCadastroKits from "../components/inputCadastroKits";
 import ButtonMedio from "../components/button";
+import {useState} from 'react';
 
 const CadastroKit: React.FC = () =>{
+    // Define a type for the chart data state
+    interface itemKit {
+        name: string
+        index: number
+        quantity: string;
+    }
+    const [kitItems,setKitItems] = useState<itemKit[]>()
+    const [num, setNumber] = useState<number>()
+    function selectionItem(item:number){
+        setNumber(item)
+    }       
+    function addItem(event: React.MouseEvent<HTMLDivElement, MouseEvent>){    
+        console.log(num);
+    }
     return (
         <>
         <div className="flex justify-center">
@@ -12,21 +27,23 @@ const CadastroKit: React.FC = () =>{
             <div className="w-full flex flex-col items-center">
                 <InputCadastroKits props="w-[760px]" text="Nome do Kit" label="Digite o nome do kit"/>
                 <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4 mt-0">
-                    <CardItem number= {1} />
-                    <CardItem number= {2}/>
-                    <CardItem number= {3}/>
-                    <CardItem number= {4}/>
-                    <CardItem number= {5}/>
-                    <CardItem number= {6}/>
-                    <CardItem number= {7}/>
-                    <CardItem number= {8}/>  
+                    <CardItem number= {1} onSelectItem={selectionItem}/>
+                    <CardItem number= {2}  onSelectItem={selectionItem}/>
+                    <CardItem number= {3}  onSelectItem={selectionItem}/>
+                    <CardItem number= {4}  onSelectItem={selectionItem}/>
+                    <CardItem number= {5}  onSelectItem={selectionItem}/>
+                    <CardItem number= {6}  onSelectItem={selectionItem}/>
+                    <CardItem number= {7}  onSelectItem={selectionItem}/>
+                    <CardItem number= {8}  onSelectItem={selectionItem}/>  
                 </div>
             <div className="mt-4 flex gap-24">
                 <InputCadastroKits props="w-72"text="Item" label="Digite o item a ser adicionado"/>
                 <InputCadastroKits props="w-72"text="Quantidade" label="Digite a quantidade desse item"/>
             </div>
             <div className="flex gap-40 mb-4">
-                <ButtonMedio title="Salvar" props="bg-blue-900 w-28"/>
+                <div onClick={addItem}>
+                    <ButtonMedio title="Salvar" props="bg-blue-900 w-28"/>
+                </div>
                 <ButtonMedio title="Cancelar" props="bg-red-500 w-36"/>
             </div>
             </div>
