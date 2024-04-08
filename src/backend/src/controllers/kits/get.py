ec7c53220b1652926d387c7785dd1dbd804e5c10
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2024-04-08 17:00:03 -0300

    feat: get all kits route returning all

--- src/backend/src/controllers/kits/get.py
+++ src/backend/src/controllers/kits/get.py
@@ -1,23 +1,41 @@
 from typing import List, Optional
 from models.kits import KitSchema
+import json
 
 from dbconnect import conn_postgres
 
 
-async def get_all() -> Optional[List[KitSchema]]:
+async def get_all():
     async with conn_postgres.transaction():
         query = """
-        SELECT * FROM kits;
+            SELECT
+                jsonb_build_object(
+                    'id', kits.id,
+                    'name', kits.name,
+                    'itens', jsonb_agg(
+                        jsonb_build_object(
+                            'item_id', itens.id,
+                            'item_name', itens.name,
+                            'item_position', kit_positions.position,
+                            'quantity', kit_positions.quantity
+                        )
+                    )
+                ) AS kit
+            FROM
+                kits
+            JOIN
+                kit_positions ON kits.id = kit_positions.kit_id
+            JOIN
+                itens ON kit_positions.item_id = itens.id
+            GROUP BY
+                kits.id;
         """
 
         rows = await conn_postgres.fetch(query)
 
-        if not rows:
-            return None
+        rows = [json.loads(row["kit"]) for row in rows]
 
-        kits = [KitSchema(**row) for row in rows]
-
-        return kits
+        return rows
 
 
 async def get_by_id(kit_id: int) -> Optional[KitSchema]:
